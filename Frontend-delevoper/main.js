dacb84b879e4b0f12f09cdb19d9c5b745198716b
Ferney Velasco Garzón <fvgytu@gmail.com>
2022-08-02 20:12:32 -0500

    I added other functions to show a product in specific about in product details aside - clearing the CSS

--- Frontend-delevoper/main.js
+++ Frontend-delevoper/main.js
@@ -13,10 +13,13 @@ const shoppingMenu = document.querySelector('.product-detail');
 
 shoppingCart.addEventListener('click', toggleShopMenu)
 
+const productPicked = document.querySelector('.product-picked');
+
 function toggleDesktopMenu() {
     menu.classList.toggle('inactive');
     // toggle can active or inactive the css class
     shoppingMenu.classList.add('inactive');
+    productPicked.classList.add('inactive');
     // this function allows to add a class new
 }
 
@@ -24,6 +27,7 @@ function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
     // toggle can active or inactive the css class
     shoppingMenu.classList.add('inactive');
+    productPicked.classList.add('inactive');
 }
 
 function toggleShopMenu() {
@@ -31,6 +35,7 @@ function toggleShopMenu() {
     // toggle can active or inactive the css class
     menu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
+    productPicked.classList.add('inactive');
 }
 
 // Add random products to select
@@ -66,37 +71,82 @@ productsList.push({
     image: 'https://images.pexels.com/photos/1431117/pexels-photo-1431117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'    
 });
 
+console.log(productsList.name)
+
 const showProducts = (array) => {
     const container = document.querySelector('.cards-container');
-    for(product of productsList) {
+    let counter = 0;
+    for(product of array) {
         const card = `
         <div class="product-card">
-            <img src="${product.image}" alt="${product.name}" class="imageProduct">
+            <img src="${product.image}" alt="${product.name}" class="imageProduct" onclick="openProductPicked${counter+1}()">
             <div class="product-info">
                 <div>
                     <p class="priceProduct">$${product.price}</p>
                     <p class="nameProduct">${product.name}</p>
                 </div>
                 <figure>
-                    <img src="./Icons/bt_add_to_cart.svg" alt="add-cart" id="saveInCart">
+                    <img src="./Icons/bt_add_to_cart.svg" alt="add-cart" id="saveInCart" onclick="addCart()">
                 </figure>
             </div>
         </div>
         `;
         container.innerHTML += card;
+        counter++;
     }
 }
 
 showProducts(productsList);
 
 
+// Functions that appears innerHTML
 
+function openProductSpecific(number) {
+    let productInnerList = productsList[number];
+    const picked = `
+        <div class="product-picked-close" onclick="productPickedClose()">
+            <img src="./Icons/icon_close.png" alt="close"> 
+        </div>
+        <img src="${productInnerList.image}" alt="${productInnerList.name}">
+        <div class="product">
+            <p>$${productInnerList.price}</p>
+            <p>${productInnerList.name}</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quibusdam placeat nemo id hic. Similique tenetur provident, adipisci, facere corporis nam molestiae, quis dignissimos eligendi quibusdam facilis ea dolores nulla?</p>
+            <button class="primary-button add-to-cart-button">
+                <img src="./Icons/bt_add_to_cart.svg" alt="add-to-cart">
+                Add to cart
+            </button>
+        </div>
+        `
+    productPicked.innerHTML = picked;
+    productPicked.classList.remove('inactive');
+    shoppingMenu.classList.add('inactive');
+    menu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}
 
+const productPickedClose = () => {
+    productPicked.classList.add('inactive');
+}
 
-
-
-
-
+function openProductPicked1() {
+    openProductSpecific(0);
+}
+function openProductPicked2() {
+    openProductSpecific(1);
+}
+function openProductPicked3() {
+    openProductSpecific(2);
+}
+function openProductPicked4() {
+    openProductSpecific(3);
+}
+function openProductPicked5() {
+    openProductSpecific(4);
+}
+function openProductPicked6() {
+    openProductSpecific(5);
+}
 
 // Add products to shopping cart
 // const cart = [];
